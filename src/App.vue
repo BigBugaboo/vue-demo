c657021a1e8e05d757f2b6c87c4ab959fc7be0b4
bugaboo <hj497304787@vip.qq.com>
2021-04-28 00:05:42 +0800

    feat: add vite

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Form :formConfig="[{ type: 'input' }, { type: 'select' }]" />
+</template>
+
+<script setup>
+import Form from './components/form.vue'
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,28 @@
+<template>
+<form>
+  <div v-for="(item, index) in formConfig"
+      :key="index">
+    <div v-if="item.type === 'select'">
+      select
+    </div>
+    <div v-if="item.type === 'input'">
+      input
+    </div>
+  </div>
+</form>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  formConfig: Array
+})
+
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
